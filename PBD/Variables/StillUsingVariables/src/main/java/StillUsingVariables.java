3b467192893d4ac50d0bc34bc08562da53f5d3d5
Zach <zachwalker89@yahoo.com>
2015-04-14 14:36:51 -0400

    first

---
+++ PBD/Variables/StillUsingVariables/src/main/java/StillUsingVariables.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author apprentice
+ */
+public class StillUsingVariables {
+    public static void main(String[] args){
+        
+        String name = "zach";
+        int graduate = 2008;
+        
+        System.out.println("My name is " + name + "  i graduated in " + graduate);
+    }
+    
+}
