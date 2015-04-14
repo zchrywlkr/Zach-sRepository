909cffc325244abd25d93b17e82c6d76ce858be2
Zach <zachwalker89@yahoo.com>
2015-04-14 15:16:18 -0400

    first

--- PBD/KeyboardInput/AskingQuestions/src/main/java/com/swcguild/askingquestions/AskingQuestions.java
+++ PBD/KeyboardInput/AskingQuestions/src/main/java/com/swcguild/askingquestions/AskingQuestions.java
@@ -18,20 +18,23 @@ public class AskingQuestions {
         
         String age;
        
-        String height;
+        String heightFeet;
         String weight;
         
         
-        System.out.println("How old are you");
+        System.out.println("How old are you?");
         age = sc.nextLine();
         
-        System.out.println("how tall are you");
-        height = sc.nextLine();
+        System.out.println("how many feet tall are you?");
+        heightFeet = sc.nextLine();
         
-        System.out.println("how much do you weight");
+        System.out.println("and how many inches?");
+        String heightInches = sc.nextLine();
+        
+        System.out.println("how much do you weight?");
         weight = sc.nextLine();
         
-        System.out.println("you are " + age + " old " + height + " tall and " + weight + " heavy");
+        System.out.println("you are " + age + " old " + heightFeet +"'" +   heightInches + "'" + " tall and " + weight + " heavy");
     }    
     
 }
