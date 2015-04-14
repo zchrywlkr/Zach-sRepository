0352f26355b2d9e6c0ee7262e9618bbec6a86eb3
Zach <zachwalker89@yahoo.com>
2015-04-14 14:53:18 -0400

    first

---
+++ PBD/Variables/YourSchedule/src/main/java/com/swcguild/yourschedule/YourSchedule.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.swcguild.yourschedule;
+
+/**
+ *
+ * @author apprentice
+ */
+public class YourSchedule {
+    public static void main(String[]args){
+        String course1 = "java";
+        String teacher1 = "Eric Ward";
+        String course2 = "nothing";
+        String teacher2 = "none";
+        
+        
+        System.out.println("+------------------------------------+");
+        System.out.println("|1|             " + course1 + "|    " + teacher1);
+        System.out.println("|2|          " + course2+ "|     " +teacher2);
+        System.out.println("+------------------------------------+");
+    }
+    
+}
