d2d42d374c99ce71ba1b10a5c37387839813d9e1
Zach <zachwalker89@yahoo.com>
2015-04-14 14:22:45 -0400

    first version

---
+++ PBD/Variables/VariablesAndNames/src/main/java/VariablesAndNames.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author apprentice
+ */
+public class VariablesAndNames
+{
+    public static void main( String[] args )
+    {
+        //delcares integer variables
+        int cars, drivers, passengers, cars_not_driven, cars_driven;
+        //delcares double variables
+        double space_in_a_car, carpool_capacity, average_passengers_per_car;
+        //adds values to empty variables
+        cars = 100;
+        space_in_a_car = 4;
+        drivers = 30;
+        passengers = 90;
+        cars_not_driven = cars - drivers;
+        cars_driven = drivers;
+        carpool_capacity = cars_driven * space_in_a_car;
+        average_passengers_per_car = passengers / cars_driven;
+
+        //prints text and variables in the console
+        System.out.println( "There are " + cars + " cars available." );
+        System.out.println( "There are only " + drivers + " drivers available." );
+        System.out.println( "There will be " + cars_not_driven + " empty cars today." );
+        System.out.println( "We can transport " + carpool_capacity + " people today." );
+        System.out.println( "We have " + passengers + " to carpool today." );
+        System.out.println( "We need to put about " + average_passengers_per_car + " in each car." );
+    }
+}
