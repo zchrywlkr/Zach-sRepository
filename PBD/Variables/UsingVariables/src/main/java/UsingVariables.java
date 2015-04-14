ad3fecf8ef03df22ac8dc26f1f4ae36bd160f130
Zach <zachwalker89@yahoo.com>
2015-04-14 14:33:07 -0400

    first

---
+++ PBD/Variables/UsingVariables/src/main/java/UsingVariables.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author apprentice
+ */
+public class UsingVariables {
+    public static void main(String[] args){
+        int integer = 113;
+        double adouble = 2.71828;
+        String string = "Comuter Science";
+        System.out.println("the integer is" + integer);
+        System.out.println("the double is " + adouble);
+        System.out.println("the string is " + string);
+    }
+   
+    
+}
