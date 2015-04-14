c2a2eec0d8b71a7448e3fabd89a2b25d86ce8d0d
Zach <zachwalker89@yahoo.com>
2015-04-14 15:23:38 -0400

    first

---
+++ PBD/KeyboardInput/TheForgetfulMachine/src/main/java/com/swcguild/theforgetfulmachine/TheForgetfulMachine.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.swcguild.theforgetfulmachine;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author apprentice
+ */
+public class TheForgetfulMachine {
+    
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("enter a word");
+        sc.nextLine();
+        System.out.println("give  me a second work");
+        sc.nextLine();
+        System.out.println("give me a number");
+        sc.nextLine();
+        System.out.println("give me another number");
+        sc.nextLine();
+        System.out.println("so much fun!!!");
+        
+    }
+    
+}
